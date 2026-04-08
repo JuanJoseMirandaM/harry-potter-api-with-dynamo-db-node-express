40419099939e33e17fa869741008761fc2e3a5a5
Juan Jose Miranda <39716343+JuanJoseMirandaM@users.noreply.github.com>
2026-04-08 01:12:18 -0400

    Change server port from 3000 to 4000

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.delete('/characters/:id', async (req, res) => {
     }
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
-    console.log(`listening on port port`);
+    console.log(`listening on port 4000`);
 });
